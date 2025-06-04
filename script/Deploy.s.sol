1526dc68bdef193123eeb92df2704c7fdb3a88d9
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2025-06-04 15:16:32 -0600

    deploy script updated but not running

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,19 +7,35 @@ import {TransparentUpgradeableProxy} from "@openzeppelin/proxy/transparent/Trans
 import {IERC20Upgradeable} from "@openzeppelin-upgradeable/interfaces/IERC20Upgradeable.sol";
 
 import "../src/ATokenVault.sol";
+import {console2} from "forge-std/Test.sol";
+
+// forge script script/Deploy.s.sol:Deploy --rpc-url $ARBITRUM_RPC_URL --private-key $PRIVATE_KEY --broadcast --verify --force --etherscan-api-key $ETHERSCAN_API_KEY -vvvv
+
+// forge script script/Deploy.s.sol:Deploy --rpc-url $POLYGON_RPC_URL --private-key $PRIVATE_KEY --broadcast --verify --force --etherscan-api-key $ETHERSCAN_API_KEY -vvvv
+
 
 contract Deploy is Script {
+
+    address constant OWNER = 0x1F3D49c350BE3e63940c22f0560eEE3c34A717F9;
+
+    address constant USDC_ADDRESS_ARBITRUM = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+    address constant POOL_ADDRESSES_PROVIDER_ADDRESS_ARBITRUM = 0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb;    
+
+    address constant USDC_ADDRESS_POLYGON = 0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359;
+    address constant POOL_ADDRESSES_PROVIDER_ADDRESS_POLYGON = 0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb;
+ 
+
     // DEPLOYMENT PARAMETERS - CHANGE THESE FOR YOUR VAULT
     // ===================================================
-    address UNDERLYING_ASSET_ADDRESS = address(0); // Underlying asset listed in the Aave Protocol
+    address UNDERLYING_ASSET_ADDRESS = USDC_ADDRESS_POLYGON; // Underlying asset listed in the Aave Protocol
     uint16 REFERRAL_CODE = 0; // Referral code to use
-    address AAVE_POOL_ADDRESSES_PROVIDER_ADDRESS = address(0); // PoolAddressesProvider contract of the Aave Pool
+    address AAVE_POOL_ADDRESSES_PROVIDER_ADDRESS = POOL_ADDRESSES_PROVIDER_ADDRESS_POLYGON; // PoolAddressesProvider contract of the Aave Pool
     address constant PROXY_ADMIN_ADDRESS = address(0); // Address of the proxy admin
-    address constant OWNER_ADDRESS = address(0); // Address of the vault owner
-    string constant SHARE_NAME = "Wrapped aDAI"; // Name of the token shares
-    string constant SHARE_SYMBOL = "waDAI"; // Symbol of the token shares
-    uint256 constant FEE = 0.1e18; // Vault Fee bps in wad (e.g. 0.1e18 results in 10%)
-    uint256 constant INITIAL_LOCK_DEPOSIT = 10e18; // Initial deposit on behalf of the vault
+    address constant OWNER_ADDRESS = OWNER; // Address of the vault owner
+    string constant SHARE_NAME = "AAVE V3 USDC"; // Name of the token shares
+    string constant SHARE_SYMBOL = "Av3USDC"; // Symbol of the token shares
+    uint256 constant FEE = 0; // Vault Fee bps in wad (e.g. 0.1e18 results in 10%)
+    uint256 constant INITIAL_LOCK_DEPOSIT = 100; // Initial deposit on behalf of the vault
     // ===================================================
 
     ATokenVault public vault;
@@ -36,11 +52,11 @@ contract Deploy is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         address deployerAddress = vm.addr(deployerPrivateKey);
-        console.log("Deployer address: ", deployerAddress);
-        console.log("Deployer balance: ", deployerAddress.balance);
-        console.log("BlockNumber: ", block.number);
-        console.log("ChainId: ", getChainId());
-        console.log("Deploying vault...");
+        console2.log("Deployer address: ", deployerAddress);
+        console2.log("Deployer balance: ", deployerAddress.balance);
+        console2.log("BlockNumber: ", block.number);
+        console2.log("ChainId: ", getChainId());
+        console2.log("Deploying vault...");
 
         require(
             INITIAL_LOCK_DEPOSIT != 0,
