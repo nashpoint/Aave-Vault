a7d39291eda518c90b9172d71c36aa59382f7c40
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2025-06-04 17:09:57 -0600

    tidy up

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -9,26 +9,29 @@ import {IERC20Upgradeable} from "@openzeppelin-upgradeable/interfaces/IERC20Upgr
 import "../src/ATokenVault.sol";
 import {console2} from "forge-std/Test.sol";
 
-// forge script script/Deploy.s.sol:Deploy --rpc-url $ARBITRUM_RPC_URL --private-key $PRIVATE_KEY --broadcast --verify --force --etherscan-api-key $ETHERSCAN_API_KEY -vvvv
+// forge script script/Deploy.s.sol:Deploy --rpc-url $ARBITRUM_RPC_URL --private-key $PRIVATE_KEY --broadcast -vvvv
 
 contract Deploy is Script {
 
     address constant OWNER = 0x1F3D49c350BE3e63940c22f0560eEE3c34A717F9;
 
-    address constant USDC_ADDRESS_ARBITRUM = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
-    address constant POOL_ADDRESSES_PROVIDER_ADDRESS_ARBITRUM = 0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb;    
+    address constant USDC_ARBITRUM = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+    address constant POOL_ADDRESSES_PROVIDER_ARBITRUM = 0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb;   
+
+    // USDC: https://arbiscan.io/address/0xaf88d065e77c8cC2239327C5EDb3A432268e5831
+    // Pool Address Provider: https://arbiscan.io/address/0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb
 
     // DEPLOYMENT PARAMETERS - CHANGE THESE FOR YOUR VAULT
     // ===================================================
-    address UNDERLYING_ASSET_ADDRESS = USDC_ADDRESS_ARBITRUM; // Underlying asset listed in the Aave Protocol
+    address UNDERLYING_ASSET_ADDRESS = USDC_ARBITRUM; // Underlying asset listed in the Aave Protocol
     uint16 REFERRAL_CODE = 0; // Referral code to use
-    address AAVE_POOL_ADDRESSES_PROVIDER_ADDRESS = POOL_ADDRESSES_PROVIDER_ADDRESS_ARBITRUM; // PoolAddressesProvider contract of the Aave Pool
+    address AAVE_POOL_ADDRESSES_PROVIDER_ADDRESS = POOL_ADDRESSES_PROVIDER_ARBITRUM; // PoolAddressesProvider contract of the Aave Pool
     address constant PROXY_ADMIN_ADDRESS = address(0); // Address of the proxy admin
     address constant OWNER_ADDRESS = OWNER; // Address of the vault owner
     string constant SHARE_NAME = "AAVE VAULT USDC"; // Name of the token shares
     string constant SHARE_SYMBOL = "avUSDC"; // Symbol of the token shares
     uint256 constant FEE = 0; // Vault Fee bps in wad (e.g. 0.1e18 results in 10%)
-    uint256 constant INITIAL_LOCK_DEPOSIT = 100; // Initial deposit on behalf of the vault
+    uint256 constant INITIAL_LOCK_DEPOSIT = 1e6; // Initial deposit on behalf of the vault
     // ===================================================
 
     ATokenVault public vault;
